144a26cfe20406a12ffdf1289c10159f1c2b98a8
Nicolas Besnard <besnard.nicolas@gmail.com>
2014-10-20 18:39:56 +0100

    Add default row to User Menu

---
+++ BahOuaisMorray/ViewControllers/UserMenuViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UserMenuViewController.swift
+//  BahOuaisMorray
+//
+//  Created by Nicolas Besnard on 20/10/2014.
+//  Copyright (c) 2014 Nicolas Besnard. All rights reserved.
+//
+
+import UIKit
+
+class UserMenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    @IBOutlet weak var tableView: UITableView!
+    
+    let menuItems = ["Ajouter un contact", "Partager", "Feedback", "Déconnexion"]
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate   = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return menuItems.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("UserMenuCell") as UITableViewCell
+        
+        cell.textLabel!.text = menuItems[indexPath.row]
+        
+        return cell
+    }
+}
